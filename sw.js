2505677ebbb21373ed69108efa5bdffe7a67f49e
Dr. Shreepati Panigrahy <shreepatip@gmail.com>
2026-09-19 18:26:26 +0530

    Support landscape orientation and sequence reordering in Media Upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cbcdr-cache-v6'; 
+const CACHE_NAME = 'cbcdr-cache-v7'; 
 const urlsToCache = ['./', './index.html', './manifest.json', './icon.png'];
 
 // Install & Force Immediate Activation
