c4b3cb84c4b094bf738da9b05673ed54a2225b9a
Dr. Shreepati Panigrahy <shreepatip@gmail.com>
2026-09-22 15:31:24 +0530

    Fix Analytics CSV button: fix strVal ReferenceError in saveDataSilently and guarantee direct file download
    - Fixed ReferenceError: strVal is not defined in saveDataSilently (which was halting exportAndShareCSV on click).
    - Made forceDownload() direct instant download execute unconditionally, fixing Web Share blocker on desktop/laptops.
    - Service Worker cache bumped to cbcdr-cache-v19.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cbcdr-cache-v18'; 
+const CACHE_NAME = 'cbcdr-cache-v19'; 
 const urlsToCache = ['./', './index.html', './manifest.json', './icon.png'];
 
 // Install & Force Immediate Activation
